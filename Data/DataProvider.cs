76c3eda3ac2e0715b8fc6cd6133902f3b4e274f4
Han Trinh <hantn16@gmail.com>
2017-11-08 22:09:30 +0700

    InitialCommit

---
+++ Data/DataProvider.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace quanlythuvien.lop_du_lieu
+{
+    public class DataProvider
+    {
+        public static string connectionString = "Data Source=.\\DATA1;Initial Catalog=QUANLYTHUVIEN_DATA;Integrated Security=True";
+        public DataTable ExecuteQuery(string query,object[] parameter = null)
+        {
+            DataTable data = new DataTable();
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand(query, connection);
+                if (parameter!=null)
+                {
+                    string[] listParams = query.Split(' ');
+                    int i = 0;
+                    foreach (string item in listParams)
+                    {
+                        if (item.StartsWith("@"))
+                        {
+                            command.Parameters.AddWithValue(item, parameter[i]);
+                            i++;
+                        }
+                    }
+                }
+                SqlDataAdapter adapter = new SqlDataAdapter(command);
+                adapter.Fill(data);
+                connection.Close();
+            }
+            return data;
+        }
+        public int ExecuteNonQuery(string query, object[] parameter = null)
+        {
+            int data = 0;
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand(query, connection);
+                if (parameter != null)
+                {
+                    string[] listParams = query.Split(' ');
+                    int i = 0;
+                    foreach (string item in listParams)
+                    {
+                        if (item.StartsWith("@"))
+                        {
+                            command.Parameters.AddWithValue(item, parameter[i]);
+                            i++;
+                        }
+                    }
+                }
+                data = command.ExecuteNonQuery();
+                connection.Close();
+            }
+            return data;
+        }
+        public Object ExecuteScalar(string query, object[] parameter = null)
+        {
+            Object data = 0;
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand(query, connection);
+                if (parameter != null)
+                {
+                    string[] listParams = query.Split(' ');
+                    int i = 0;
+                    foreach (string item in listParams)
+                    {
+                        if (item.StartsWith("@"))
+                        {
+                            command.Parameters.AddWithValue(item, parameter[i]);
+                            i++;
+                        }
+                    }
+                }
+
+                data = command.ExecuteScalar();
+                connection.Close();
+            }
+            return data;
+        }
+    }
+}
